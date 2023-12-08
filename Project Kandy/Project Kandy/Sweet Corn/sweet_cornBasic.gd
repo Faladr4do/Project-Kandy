5526c6f486d9815b84263c7f07fda0eb41edd547
Faladr4do <152029206+Faladr4do@users.noreply.github.com>
2023-12-08 23:15:56 +0000

    Criação de inimigo / Organizar
    Criei um inimigo que para por 3 segundos antes de se virar(ainda em desenvolvimento).
    Alem disso, organizei os ficheiros do projeto.

---
+++ Project Kandy/Project Kandy/Sweet Corn/sweet_cornBasic.gd
@@ -0,0 +1,93 @@
+extends CharacterBody2D
+
+
+@onready var velocy = -20.0
+@onready var velocyOld = 0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var estaVivo = true
+var estaMorrer = false
+var patrulhando = false
+
+
+@onready var animCorn = $AnimationPlayer
+@onready var spriteCorn = $Sprite2D
+@onready var chao = $RayCast2D
+@onready var alvo = $target
+
+func _ready():
+	add_to_group("Inimigo")
+	alvo.add_to_group("Alvo")
+	
+
+func _physics_process(delta):
+	if estaMorrer:
+		return
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	
+	if is_on_floor():
+		if chao.is_colliding() and !is_on_wall():
+			velocity.x = velocy
+		elif !chao.is_colliding():
+			virar()
+#	if is_on_floor() or is_on_wall():
+#		if chao.is_colliding() and !is_on_wall():
+#			velocity.x = velocy
+#		elif !chao.is_colliding() or is_on_wall():
+#			virar()
+	
+	move_and_slide()
+	atualizar_Anims(velocy)
+	fall()
+	
+func atualizar_Anims(velocy):
+	if estaMorrer:
+		return
+	if is_on_floor():
+		if velocy == 0:
+			animCorn.play("idle")
+		else:
+			animCorn.play("walk")
+	else:
+		if velocity.y < 0:
+			animCorn.play("jump")
+		#elif position.y > 200:
+			#animCorn.play("dead")
+
+func fall():
+	if not is_on_floor() and position.y > 200:
+		animCorn.play("dead")
+
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("Vegetal"):
+		morte()
+
+func _on_target_body_entered(body):
+	if body.is_in_group("Bala"):
+		morte()
+	elif body.is_in_group("Vegetal"):
+		virar()
+
+func _on_teste_parede_body_entered(body):
+	if body.is_in_group("Vegetal") or body.is_in_group("Bala"):
+		pass
+	else:
+		virar()
+
+
+func morte():
+	if estaMorrer:
+		return
+	animCorn.play("dead")
+	$hitbox/CollisionShape2D.call_deferred("set_disabled", true)
+	$CollisionShape2D.call_deferred("set_disabled", true)
+	estaMorrer = true
+	await get_tree().create_timer(0.75).timeout
+	queue_free()
+
+func virar():
+	scale.x = abs(scale.x) * -1
+	velocy= velocy*-1
