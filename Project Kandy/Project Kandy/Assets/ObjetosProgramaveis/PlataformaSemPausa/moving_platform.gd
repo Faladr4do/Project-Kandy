7511613adef95241f956faffcd2214c5309309d1
Faladr4do <152029206+Faladr4do@users.noreply.github.com>
2023-12-08 23:22:20 +0000

    Mais organização

---
+++ Project Kandy/Project Kandy/Assets/ObjetosProgramaveis/PlataformaSemPausa/moving_platform.gd
@@ -0,0 +1,18 @@
+extends Path2D
+
+@export var loop = true
+@export var velocidade = 10
+
+@onready var caminho = $PathFollow2D
+@onready var animacion = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if loop:
+		animacion.play("move")
+		set_process(false)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	caminho.progress += velocidade
