65be39309befbe880e2f711eb6153def89777f7d
Ricardo Lila <152029206+Faladr4do@users.noreply.github.com>
2023-12-13 12:09:42 +0000

    Correção Importante
    Consegui corrigir toda a escala para ter zoom normal e todos os objetos 8 vezes maiores. E corrigi a gravidade e variaveis de movimento.
    Alem disso criei um menu como start page e terei de corrigir a resolução dos textos.

--- Project Kandy/Project Kandy/Projeteis/fireball.gd
+++ Project Kandy/Project Kandy/Projeteis/fireball.gd
@@ -1,13 +1,20 @@
 extends Area2D
 
 #var direcao = Vector2.RIGHT
-var velocidade = -90
+var velocidade
+
+@onready var sprite_estigada = $Sprite2D
 
 func _ready():
 	add_to_group("Bala")
 
 func _physics_process(delta):
 	position += transform.x * velocidade * delta
+	if velocidade < 0:
+		sprite_estigada.flip_h= true
+		sprite_estigada.rotation_degrees = 21.8
+	else:
+		pass
 	
 func _on_body_entered(body):
 	if body.is_in_group("Inimigo"):
--- Project Kandy/global.gd
+++ Project Kandy/global.gd
@@ -2,3 +2,4 @@ extends Node
 
 var moedas_coletadas = 0
 var vidas_totais = 3
+var velocidade_total = 1
