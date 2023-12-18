79210185c67546d43b7eca98868c509cadfc764f
Ricardo Lila <152029206+Faladr4do@users.noreply.github.com>
2023-12-18 15:06:35 +0000

    Aula de Programação
    Tentei animar o Leo e tals

--- Project Kandy/Project Kandy/DrPedra/dr_pedra.gd
+++ Project Kandy/Project Kandy/DrPedra/dr_pedra.gd
@@ -5,9 +5,12 @@ const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var esta_pestanejar = false
+var esta_respirar = true
+var esta_falar = false
 
 @onready var local_falar = $Marker2D.global_position
+@onready var animPedra = $AnimationPlayer
+@onready var spritePedra = $Sprite2D
 
 const falas: Array[String] = [
 	"Bons dias jovem",
@@ -19,13 +22,30 @@ func _physics_process(delta):
 	# Add the gravity.
 	if !is_on_floor():
 		velocity.y += gravity * delta
+	if is_on_floor():
+		if esta_respirar:
+			animPedra.play("idle_respirar")
+			
+#		elif !esta_respirar:
+#			animPedra.play("idle_pestanejar")
+#
+		elif esta_falar:
+			animPedra.play("falar")
+			
 	move_and_slide()
 
 func _on_area_dialogo_body_entered(body):
 	if body.is_in_group("Vegetal"):
+		esta_falar = true
 		Dialog.comecar_dialogo(local_falar, falas)
+		esta_falar = false
+	else:
+		esta_falar = false
 
 
 func _on_area_voltar_dr_body_entered(body):
 	if body.is_in_group("Vegetal"):
 		scale.x = abs(scale.x) * -1
+		
+func pestanejar():
+	pass
