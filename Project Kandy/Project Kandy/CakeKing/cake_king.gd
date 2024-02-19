e9b00a1b48bd4c68ef7fd0064396a961ddaef6cc
Ricardo Lila <152029206+Faladr4do@users.noreply.github.com>
2024-02-19 15:06:45 +0000

    CakeKing

---
+++ Project Kandy/Project Kandy/CakeKing/cake_king.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	move_and_slide()
