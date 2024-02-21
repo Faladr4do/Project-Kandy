ddb6131a8543ac6c02acc5d4b9d5ee3cf81e40e8
Ricardo Lila <152029206+Faladr4do@users.noreply.github.com>
2024-02-21 12:54:57 +0000

    Boss Map / World Environment Tests

--- Project Kandy/Project Kandy/Assets/CameraPlayer/camera_player.gd
+++ Project Kandy/Project Kandy/Assets/CameraPlayer/camera_player.gd
@@ -9,17 +9,18 @@ extends Camera2D
 @export var mapa_nivel : TileMap
 
 func _ready():
-	var rect_mapa = mapa_nivel.get_used_rect()
-	var tamanho_tile = mapa_nivel.cell_quadrant_size
-	var tamanho_mapa_pixel = rect_mapa.size * tamanho_tile
-	limit_right = tamanho_mapa_pixel.x * escala_direita
-	limit_left = -tamanho_mapa_pixel.x * escala_esquerda
-	limit_bottom = tamanho_mapa_pixel.y * escala_baixo
-	position_smoothing_enabled = smooth_camera
-	camera(zoom_camera)
-	print_debug(limit_right)
-	print_debug(limit_left)
-	print_debug(limit_bottom)
+	if mapa_nivel:
+		var rect_mapa = mapa_nivel.get_used_rect()
+		var tamanho_tile = mapa_nivel.cell_quadrant_size
+		var tamanho_mapa_pixel = rect_mapa.size * tamanho_tile
+		limit_right = tamanho_mapa_pixel.x * escala_direita
+		limit_left = -tamanho_mapa_pixel.x * escala_esquerda
+		limit_bottom = tamanho_mapa_pixel.y * escala_baixo
+		position_smoothing_enabled = smooth_camera
+		camera(zoom_camera)
+		print_debug(limit_right)
+		print_debug(limit_left)
+		print_debug(limit_bottom)
 
 func camera(zoom):
 	zoom = Vector2(zoom_camera, zoom_camera)
